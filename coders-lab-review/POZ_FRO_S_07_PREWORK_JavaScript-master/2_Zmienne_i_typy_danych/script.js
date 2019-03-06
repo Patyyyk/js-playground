0b1e6753c12f6f0742b91bae14b82886a10a9d6a
patyyyk <patyyyk@gmail.com>
2019-03-06 21:03:31 +0100

    3_prework zaczac operatory

--- coders-lab-review/POZ_FRO_S_07_PREWORK_JavaScript-master/2_Zmienne_i_typy_danych/script.js
+++ coders-lab-review/POZ_FRO_S_07_PREWORK_JavaScript-master/2_Zmienne_i_typy_danych/script.js
@@ -1,1 +1,39 @@
+var number = 12;
+var word = 'Pioter';
+var wordNumber = (2 + "dwa")
+var logic = true;
+var special = null;
 
+
+console.log(number);
+console.log(typeof(number));
+
+console.log(word);
+console.log(typeof(word));
+
+console.log(wordNumber);
+console.log(typeof(wordNumber));
+
+console.log(logic);
+console.log(typeof(logic));
+
+console.log(special);
+console.log(typeof(special));
+
+// zadanie 2
+console.log('/////////////////////////////////////////////');
+
+var numberOne = 2;
+var numberTwo = 10;
+var result = 0;
+
+result = numberOne + numberTwo;
+
+console.log(result);
+
+// zadanie 3
+console.log('/////////////////////////////////////////////');
+
+var temp = 
+
+console.log(temp);
