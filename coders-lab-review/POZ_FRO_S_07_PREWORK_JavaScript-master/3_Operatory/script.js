ebfac267daee91c68e9e547d6881864a7d17a3da
patyyyk <patyyyk@gmail.com>
2019-03-10 10:24:28 +0100

    Exercises

--- coders-lab-review/POZ_FRO_S_07_PREWORK_JavaScript-master/3_Operatory/script.js
+++ coders-lab-review/POZ_FRO_S_07_PREWORK_JavaScript-master/3_Operatory/script.js
@@ -1,1 +1,56 @@
+console.log('Zad1');
+var good = true;
+var bad = false;
 
+console.log(good == bad);
+
+console.log('Zad2');
+
+var firsNumber = 6,
+    secondNumber = 4,
+    resultModulo = 0;
+
+  resultModulo = firsNumber % secondNumber
+
+  console.log(resultModulo);
+
+console.log('Zad3');
+
+var firsWord = "Piotr",
+    secondWord = "Adamek",
+    joinedStrings = '';
+
+joinedStrings = firsWord + secondWord;
+console.log(joinedStrings);
+
+console.log('Zad4');
+
+var myNumber = 4,
+    myString = "4";
+
+console.log(myNumber == myString);
+console.log(myNumber === myString);
+
+console.log('Zad5');
+
+var counter = 145;
+
+console.log(counter);
+
+counter = counter + 1;
+
+console.log(counter);
+
+counter = counter - 1;
+
+console.log(counter);
+
+console.log('Zad6');
+
+var greaterNumer = 10,
+    lowerMumber = 5,
+    result = null;
+
+result = greaterNumer > lowerMumber;
+
+console.log(result);
